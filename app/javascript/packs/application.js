be9062ac3ceeaded1aab077d835551ae56b56894
Juan Estrella <juan.estrella137@gmail.com>
2020-03-26 16:09:34 +0100

    Add form-display functionality

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,7 @@
 import "bootstrap";
 import {initAutocomplete} from "../plugins/init_autocomplete";
+import {initHangoutForm} from "../components/hangout_form";
 import "../plugins/flatpickr";
 initAutocomplete();
 
+initHangoutForm();
