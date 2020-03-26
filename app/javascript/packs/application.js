e2e6981fa247d03e39ab2a17c32cf342f46a7f98
Juan Estrella <Juan.estrella137@gmail.com>
2020-03-26 16:14:03 +0100

    Revert "Form display"

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,5 @@
 import "bootstrap";
 import {initAutocomplete} from "../plugins/init_autocomplete";
-import {initHangoutForm} from "../components/hangout_form";
 import "../plugins/flatpickr";
 initAutocomplete();
 
-initHangoutForm();
