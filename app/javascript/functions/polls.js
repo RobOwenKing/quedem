fa5e53c734926f2ad9b7b0c886b00bbdb4fa4176
Robert King <robert.o.king@hotmail.co.uk>
2020-03-26 16:09:43 +0100

    Working event listeners on date choices

--- app/javascript/functions/polls.js
+++ app/javascript/functions/polls.js
@@ -1,1 +1,18 @@
-polls.js
+const dateChoices = document.querySelectorAll(".card-date");
+const dateInput = document.getElementById("date-votes");
+
+const createDateListeners = () => {
+  dateChoices.forEach(date => {
+    date.addEventListener('click', (event) => {
+      date.classList.toggle("checked");
+
+      if (dateInput.value === "") {
+        dateInput.value = date.dataset.id;
+      } else {
+        dateInput.value += `,${date.dataset.id}`;
+      }
+    });
+  });
+};
+
+export { createDateListeners };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,5 @@
 import "bootstrap";
-import {initAutocomplete} from "../plugins/init_autocomplete"
-initAutocomplete()
+import {initAutocomplete} from "../plugins/init_autocomplete";
+initAutocomplete();
+import {createDateListeners} from "../functions/polls";
+createDateListeners();
