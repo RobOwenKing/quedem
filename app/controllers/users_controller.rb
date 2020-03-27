d134f57cee268a62da3cf001d64ca8c2d6182d00
Robert King <robert.o.king@hotmail.co.uk>
2020-03-27 14:55:58 +0100

    Attendances now work (fingers crossed)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :create, :update ]
 
   def create
-    raise
     user = current_user || User.create(name: params[:name], password: params[:password], email: params[:email] )
     dates = params[:date_votes]
     dates.split(",").each do |date|
@@ -13,13 +12,12 @@ class UsersController < ApplicationController
   end
 
   def update
-    raise
     user = current_user || User.create(name: params[:name], password: params[:password], email: params[:email] )
     dates = params[:date_votes]
     dates.split(",").each do |date|
       DateVote.create(date_choice_id: date, user: user)
     end
     rsvp = params[:attendance]
-    Attendance.create(user: user, hangout_id: params[:hangout_id], response: rsvp )
+    Attendance.create(user: user, hangout_id: params[:user][:hangout_id].to_i, response: rsvp )
   end
 end
