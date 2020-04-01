64c9084c0e97b4452ca8987fee99cc69589ce723
Ry Ellingson <rycellingson@gmail.com>
2020-04-01 18:55:18 +0200

    set default url options in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,8 @@ class ApplicationController < ActionController::Base
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :photo])
   end
+
+  def default_url_options
+  { host: ENV["https://www.goquedem.com/"] || "localhost:3000" }
+  end
 end
